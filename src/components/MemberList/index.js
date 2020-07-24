fb24ff9df22c2c63742cf7df8fd03af207d0e288
Jhumur Chatterjee <jhumurchatterjee1996@gmail.com>
2020-07-25 02:40:03 +0530

    Adding filter option for member list

--- src/components/MemberList/index.js
+++ src/components/MemberList/index.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import Member from '../Member';
 
 function MemberList({ members }) {
-  const memberList = members.map(member => {
+  const [queryText, setQueryText] = useState('');
+  let filteredMemberList = null;
+
+  if (queryText.length) {
+    filteredMemberList = members.filter(member => member.real_name.toLowerCase().includes(queryText.trim().toLowerCase()));
+  } else {
+    filteredMemberList = members;
+  }
+
+  const memberList = filteredMemberList.map(member => {
     return <Member key={member.id.toString()} id={member.id} name={member.real_name} />;
   });
 
   return (
     <section>
-      <h2 className='mb-4'>Member List</h2>
+      <div className='d-flex justify-content-between mb-4'>
+        <h2 className='mb-0'>Member List</h2>
+
+        <form className='form-inline my-2 my-lg-0'>
+          <input
+            className='form-control'
+            type='search'
+            placeholder='Search'
+            aria-label='Search'
+            value={queryText}
+            onChange={e => setQueryText(e.target.value)}
+          />
+        </form>
+      </div>
 
       {memberList}
     </section>
@@ -18,7 +40,12 @@ function MemberList({ members }) {
 }
 
 MemberList.propTypes = {
-  members: PropTypes.array.isRequired
+  members: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      real_name: PropTypes.string.isRequired
+    }).isRequired
+  ).isRequired
 }
 
 MemberList.defaultProps = {
