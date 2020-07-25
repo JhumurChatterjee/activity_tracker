c7e6f99bdc5f2c551367d9669b8bc4d1d61d030b
Jhumur Chatterjee <jhumurchatterjee1996@gmail.com>
2020-07-25 15:25:11 +0530

    Adding modal for today's activity

--- src/components/Member/index.js
+++ src/components/Member/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Modal from '../Modal';
+
 function Member({ id, name }) {
+
   return (
     <div className='card mb-2'>
-      <div className='card-body'>
+      <div className='card-body d-flex justify-content-between'>
         <h4 className='mb-0'>{name}</h4>
+
+        <section>
+          <button type='button' className='btn btn-primary btn-sm mr-1' data-toggle='modal' data-target='#exampleModal'>
+            Today's Activities
+          </button>
+          <button type='button' className='btn btn-secondary btn-sm'>All Activities</button>
+        </section>
+
+        <Modal memberId={id} />
       </div>
     </div>
   );
